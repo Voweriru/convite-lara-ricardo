9395d7646677143b03d8dba78773dc78b4aef486
Ricardo Xavier <ricardox964@gmail.com>
2025-11-21 18:35:17 -0100

    fix another typo

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ function personalizeInvitation() {
 
     if (guestName) {
         const decodedName = decodeURIComponent(guestName);
-        document.getElementById('invitation-title').textContent = `${decodedName}, você está convidado!`;
+        document.getElementById('invitation-title').textContent = `${decodedName}, você está convidado/a!`;
     }
 }
 
